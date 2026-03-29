f0af5bec08ed14193f9e4e578ceb0b55dbd5ee73
Matheus Fábio <matheusfabiors@gmail.com>
2026-03-29 01:24:29 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+   <main className='bg-red-700'>
+    <h1 className='text-2xl'>Products</h1>
+   </main>
+  )
+}
+
+export default App
