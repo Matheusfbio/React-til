2b0ed1b6cc9262640f24c31b7c5456cdaf17a0db
Matheus Fabio <83082876+Matheusfbio@users.noreply.github.com>
2026-03-30 19:26:01 -0300

    Update useProducts.ts

--- features/product/hooks/useProducts.ts
+++ features/product/hooks/useProducts.ts
@@ -26,28 +26,3 @@ const useProducts = () => {
 
 export default useProducts;
 
-// const useProducts = () => {
-//   const [products, setProducts] = useState<any[]>([])
-//   const [loading, setLoading] = useState(true)
-
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       setLoading(true)
-//       try {
-//         const response = await fetch('/api/products')
-//         const data = await response.json()
-//         setProducts(data)
-//       } catch (error) {
-//         console.error('Error fetching products:', error)
-//       } finally {
-//         setLoading(false)
-//       }
-//     }
-
-//     fetchProducts()
-//   }, [])
-
-//   return { products, loading }
-// }
-
-// export default useProducts
